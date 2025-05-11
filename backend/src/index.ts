949d1a982e4db16be200ae3060b96296d035f590
Roman Kovban <kovban.developer@gmail.com>
2025-05-11 20:49:20 +0300

    Add express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+import express from 'express'
+
+const expressApp = express()
+
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+expressApp.listen(3000, () => {
+  console.log('Server started on port 3000')
+})
