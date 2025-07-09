7fbfe521dfc6e2cec6c023beb126a76a1fe54a03
Roman Kovban <kovban.developer@gmail.com>
2025-07-09 21:54:53 +0300

    Add scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
 import { ViewIdeaPage } from './pages/ViewIdeaPage'
-import { getAllIdeasRoute, getViewIdeaRoute } from './lib/routes'
+import * as routes from './lib/routes'
+import { Layout } from './components/Layout'
+import './styles/global.scss'
+import { NewIdeaPage } from './pages/NewIdeaPage'
 
 export const App = () => {
   return (
     <TrpcProvider>
       <BrowserRouter>
         <Routes>
-          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
-          <Route path={getViewIdeaRoute({ ideaNick: ':ideaNick' })} element={<ViewIdeaPage />} />
+          <Route element={<Layout />}>
+            <Route path={routes.getAllIdeasRoute()} element={<AllIdeasPage />} />
+            <Route path={routes.getNewIdeaRoute()} element={<NewIdeaPage />} />
+            <Route path={routes.getViewIdeaRoute(routes.viewIdeaRouteParams)} element={<ViewIdeaPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </TrpcProvider>
